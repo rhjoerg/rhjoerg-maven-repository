887740da154896b64d4ad09e63d67bd75a996602
rhjoerg <rhjoerg@gmail.com>
2020-10-24 09:13:42 +0200

    Starting to setup server

--- rhjoerg-maven-repository-standalone/src/main/java/ch/rhjoerg/maven/repository/standalone/MavenRepositoryServer.java
+++ rhjoerg-maven-repository-standalone/src/main/java/ch/rhjoerg/maven/repository/standalone/MavenRepositoryServer.java
@@ -1,9 +1,21 @@
 package ch.rhjoerg.maven.repository.standalone;
 
+import org.eclipse.jetty.server.Server;
+
 public class MavenRepositoryServer
 {
 	public static void main(String[] args)
 	{
+		Server server = new Server(6000);
 
+		try
+		{
+			server.start();
+			server.join();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
