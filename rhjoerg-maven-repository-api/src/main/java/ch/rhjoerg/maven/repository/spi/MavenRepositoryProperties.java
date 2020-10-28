6cd81d84a6a801291d5513651a809742f7f7dd62
rhjoerg <rhjoerg@gmail.com>
2020-10-28 10:42:50 +0100

    data path configuration

--- rhjoerg-maven-repository-api/src/main/java/ch/rhjoerg/maven/repository/spi/MavenRepositoryProperties.java
+++ rhjoerg-maven-repository-api/src/main/java/ch/rhjoerg/maven/repository/spi/MavenRepositoryProperties.java
@@ -2,8 +2,19 @@ package ch.rhjoerg.maven.repository.spi;
 
 public interface MavenRepositoryProperties
 {
+	public static final String PORT_KEY = "rhjoerg-maven-repository.port";
+	public static final String PORT_VALUE = "80";
+
+	public static final String DATA_KEY = "rhjoerg-maven-repository.data";
+	public static final String DATA_VALUE = "/var/rhjoerg-maven-repository";
+
 	public static int getPort()
 	{
-		return Integer.parseInt(System.getProperty("rhjoerg-maven-repository.port", "80"));
+		return Integer.parseInt(System.getProperty(PORT_KEY, PORT_VALUE));
+	}
+
+	public static String getData()
+	{
+		return System.getProperty(DATA_KEY, DATA_VALUE);
 	}
 }
