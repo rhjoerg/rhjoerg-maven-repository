757d0ff6302baa5b29a8249d61c41ad3edd34cf8
rhjoerg <rhjoerg@gmail.com>
2020-10-28 10:22:40 +0100

    added MavenRepositoryProperties

---
+++ rhjoerg-maven-repository-api/src/main/java/ch/rhjoerg/maven/repository/spi/MavenRepositoryProperties.java
@@ -0,0 +1,9 @@
+package ch.rhjoerg.maven.repository.spi;
+
+public interface MavenRepositoryProperties
+{
+	public static int getPort()
+	{
+		return 80;
+	}
+}
--- rhjoerg-maven-repository-standalone/src/main/java/ch/rhjoerg/maven/repository/standalone/MavenRepositoryServer.java
+++ rhjoerg-maven-repository-standalone/src/main/java/ch/rhjoerg/maven/repository/standalone/MavenRepositoryServer.java
@@ -1,36 +1,20 @@
 package ch.rhjoerg.maven.repository.standalone;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ListenerHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import ch.rhjoerg.maven.repository.context.loader.ServiceLoaderMavenRepositoryContext;
 import ch.rhjoerg.maven.repository.spi.MavenRepositoryContext;
+import ch.rhjoerg.maven.repository.spi.MavenRepositoryProperties;
 import ch.rhjoerg.maven.repository.web.MavenRepositoryContextListener;
 import ch.rhjoerg.maven.repository.web.MavenRepositoryServlet;
 
 public class MavenRepositoryServer
 {
-	public static final Integer DEFAULT_PORT = 8000;
-
-	public static CommandLine parseCommandLine(String[] args) throws ParseException
+	private static Server createServer()
 	{
-		Options options = new Options();
-		CommandLineParser parser = new DefaultParser();
-
-		options.addOption("p", "port", true, "port");
-
-		return parser.parse(options, args);
-	}
-
-	public static Server createServer(int port)
-	{
-		Server server = new Server(port);
+		Server server = new Server(MavenRepositoryProperties.getPort());
 		ServletContextHandler context = new ServletContextHandler();
 
 		context.setContextPath("/");
@@ -45,13 +29,10 @@ public class MavenRepositoryServer
 
 	public static void main(String[] args) throws Exception
 	{
-		CommandLine cmd = parseCommandLine(args);
-		int port = Integer.parseInt(cmd.getOptionValue("port", DEFAULT_PORT.toString()));
-
-		Server server = createServer(port);
+		Server server = createServer();
 
 		server.start();
-		System.out.println("http://localhost:" + port);
+		System.out.println(server.getURI());
 		server.join();
 	}
 }
